abb0766d7ad73587398b67033e34ae21c6c9444d
es3442 <77157003+es3442@users.noreply.github.com>
2021-10-29 00:58:15 +0900

    10/29btn animataion update

--- index.js
+++ index.js
@@ -1,58 +1,95 @@
-var color="pink"
-var startclicknum=0
+var color = "pink"
+var startclicknum = 0
+
+
 function toggleImgstart() {
-    startclicknum+=1
-    if(startclicknum%2==0){
-        document.getElementById("startbtn").src = "buttons/startbutton-"+color+".png";
-    }else{
-        document.getElementById("startbtn").src = "buttons/startbutton2-"+color+".png";
+    startclicknum += 1
+    if (startclicknum % 2 == 0) {
+        document
+            .getElementById("startbtn")
+            .src = "buttons/startbutton-" + color + ".png";
+    } else {
+        document
+            .getElementById("startbtn")
+            .src = "buttons/startbutton2-" + color + ".png";
     }
 }
 
-var endclicknum=0
+var endclicknum = 0
 function toggleImgend() {
-    endclicknum+=1
-    if(endclicknum%2==0){
-        document.getElementById("endbtn").src = "buttons/endbutton-"+color+".png";
-    }else{
-        document.getElementById("endbtn").src = "buttons/endbutton2-"+color+".png";
+    endclicknum += 1
+    if (endclicknum % 2 == 0) {
+        document
+            .getElementById("endbtn")
+            .src = "buttons/endbutton-" + color + ".png";
+    } else {
+        document
+            .getElementById("endbtn")
+            .src = "buttons/endbutton2-" + color + ".png";
     }
 }
-var playclicknum=0
+var playclicknum = 0
 function toggleImgplay() {
-    playclicknum+=1
-    if(playclicknum%2==0){
-        document.getElementById("playbtn").src = "buttons/playbutton-"+color+".png";
-    }else{
-        document.getElementById("playbtn").src = "buttons/pausebutton-"+color+".png";
+    playclicknum += 1
+    if (playclicknum % 2 == 0) {
+        document
+            .getElementById("playbtn")
+            .src = "buttons/playbutton-" + color + ".png";
+    } else {
+        document
+            .getElementById("playbtn")
+            .src = "buttons/pausebutton-" + color + ".png";
     }
 }
 
-function hoverdescription(){
-    document.getElementById("unseen1").style.display="block";
-    document.getElementById("unseen2").style.display="block";
+function hoverdescription() {
+    document
+        .getElementById("unseen1")
+        .style
+        .display = "block";
+    document
+        .getElementById("unseen2")
+        .style
+        .display = "block";
 }
-function nohoverdescription(){
-    document.getElementById("unseen1").style.display="none";
-    document.getElementById("unseen2").style.display="none";
+function nohoverdescription() {
+    document
+        .getElementById("unseen1")
+        .style
+        .display = "none";
+    document
+        .getElementById("unseen2")
+        .style
+        .display = "none";
 }
 
-timestampbtnclicknum=0;
-function clicktimestamp(){
-    timestampbtnclicknum+=1;
-    if(timestampbtnclicknum%2==0){
-        document.getElementById("timestampbtn").title="click to open timestamp";
-        document.getElementById("timestampbtn").style.transform="rotate(0deg)";
-        document.getElementById("timestampbtn").style.backgroundColor="var(--color-pink)";
-        document.getElementById("timestampbtn__icon").style.color="var(--color-dark-pink)";
+timestampbtnclicknum = 0;
+function clicktimestamp() {
+    timestampbtnclicknum += 1;
+    if (timestampbtnclicknum % 2 == 0) {
+        document.getElementById("timestampbtn").title = "click to open timestamp";
+        document.getElementById("timestampbtn").style.transform = "rotate(0deg)";
+        document.getElementById("timestampbtn").style.backgroundColor = "var(--color-pink)";
+        document.getElementById("timestampbtn__icon").style.color = "var(--color-dark-pink)";
 
-        document.getElementById("realtimestamp").style.display="none";
-    }else{
-        document.getElementById("timestampbtn").title="click to add new timestamp";
-        document.getElementById("timestampbtn").style.transform="rotateY(180deg)";
-        document.getElementById("timestampbtn").style.backgroundColor="var(--color-dark-pink)";
-        document.getElementById("timestampbtn__icon").style.color="var(--color-pink)";
+        document.getElementById("realtimestamp").style.display = "none";
+    } else {
+        document.getElementById("timestampbtn").title = "click to add new timestamp";
+        document.getElementById("timestampbtn").style.transform = "rotateY(180deg)";
+        document.getElementById("timestampbtn").style.backgroundColor = "var(--color-dark-pink)";
+        document.getElementById("timestampbtn__icon").style.color = "var(--color-pink)";
 
-        document.getElementById("realtimestamp").style.display="block";
+        document.getElementById("realtimestamp").style.display = "block";
     }
 }
+
+/* 버튼 클릭 이벤트 */
+document.getElementById('playbtn').addEventListener('click', toggleImgplay);
+document.getElementById('startbtn').addEventListener('click', toggleImgstart);
+//document.getElementById('codebtn').addEventListener('click', toggleImgcode);
+document.getElementById('endbtn').addEventListener('click', toggleImgend);
+document.getElementById('timestampbtn').addEventListener('click', clicktimestamp);
+
+/* hover 클릭 이벤트 */
+document.getElementById('questionbtn').addEventListener('mouseover', hoverdescription);
+document.getElementById('questionbtn').addEventListener('mouseout', nohoverdescription);
